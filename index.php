eb1030a4b09a9f92e346f5c0b3ddd22f47be349f
manuvc2000 <ecretu@eeepmad.com>
2021-02-16 15:48:45 +0100

    testFinal

--- index.php
+++ index.php
@@ -6,33 +6,35 @@
     <title>Document</title>
 </head>
 <body>
-<?
-class Caja{
-var $alto;
-var $ancho;
-var $largo;
-var $contenido;
-var $color;
+<?php
 
-function introduce($cosa){
-$this->contenido = $cosa;
-}
+echo "Esto es una prueba de CICD";
+// class Caja{
+// var $alto;
+// var $ancho;
+// var $largo;
+// var $contenido;
+// var $color;
 
-function muestra_contenido(){
- $this->contenido;
-}
+// function introduce($cosa){
+// $this->contenido = $cosa;
+// }
 
-}
-$micaja = new Caja;
-echo "Con esto hemos creado, o mejor dicho, instanciado, un objeto de la clase Caja llamado $micaja."
-$micaja->introduce("algo");
-$micaja->muestra_contenido();
-echo "Con estas dos sentencias estamos introduciendo "'algo'" en la caja y luego estamos mostrando ese
-contendido en el texto de la página. Nos fijamos que los métodos de un objeto se llaman utilizando el
-código ->."
-echo "nombre_del_objeto->nombre_de_metodo()"
-echo "Para acceder a los atributos de una clase también se accede con el código ->. De esta forma:
-nombre_del_objeto->nombre_del_atributo"
+// function muestra_contenido(){
+//  $this->contenido;
+// }
+
+// }
+// $micaja = new Caja;
+// echo "Con esto hemos creado, o mejor dicho, instanciado, un objeto de la clase Caja llamado $micaja."
+// $micaja->introduce("algo");
+// $micaja->muestra_contenido();
+// echo "Con estas dos sentencias estamos introduciendo "'algo'" en la caja y luego estamos mostrando ese
+// contendido en el texto de la página. Nos fijamos que los métodos de un objeto se llaman utilizando el
+// código ->."
+// echo "nombre_del_objeto->nombre_de_metodo()"
+// echo "Para acceder a los atributos de una clase también se accede con el código ->. De esta forma:
+// nombre_del_objeto->nombre_del_atributo"
 ?>
 </body>
 </html>
